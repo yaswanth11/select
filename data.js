97a9797860975b15cfad1a8a72105a3285b39c52
yaswanth11 <yaswanth.114@gmail.com>
2019-12-15 10:57:09 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,158 @@
+export const Data = [
+  {
+    provider: 'Airtel',
+    state: 'Telangana',
+    person: [
+      {
+        name: 'Richard',
+        City: 'Hyderabad',
+        phone: '987654321'
+      },
+      {
+        name: 'Tom',
+        City: 'Warangal',
+        phone: '2345678901'
+      }
+    ]
+  },
+  {
+    provider: 'BSNL',
+    state: 'Telangana',
+    person: [
+      {
+        name: 'Lee',
+        City: 'tirupati',
+        phone: '999999999'
+      },
+      {
+        name: 'Omar',
+        City: 'vizag',
+        phone: '555555555'
+      }
+    ]
+  },
+  {
+    provider: 'Airtel',
+    state: 'Karnataka',
+    person: [
+      {
+        name: 'maaya',
+        City: 'Bangalore',
+        phone: '2345617230'
+      },
+      {
+        name: 'NIcole',
+        City: 'KR Puram',
+        phone: '4567292021'
+      }
+    ]
+  },
+  {
+    provider: 'Jio',
+    state: 'Karnataka',
+    person: [
+      {
+        name: 'maaya',
+        City: 'Bangalore',
+        phone: '2345617230'
+      },
+      {
+        name: 'NIcole',
+        City: 'KR Puram',
+        phone: '4567292021'
+      }
+    ]
+  },
+  {
+    provider: 'Jio',
+    state: 'Andhra Pradesh',
+    person: [
+      {
+        name: 'Vivek',
+        City: 'Nellore',
+        phone: '2345617230'
+      },
+      {
+        name: 'Ram',
+        City: 'MPL',
+        phone: '4567292021'
+      }
+    ]
+  },
+  {
+    provider: 'Jio',
+    state: 'Telangana',
+    person: [
+      {
+        name: 'Harish',
+        City: 'Hyderabad',
+        phone: '2345617230'
+      },
+      {
+        name: 'Monica',
+        City: 'Ameerpet',
+        phone: '4567292021'
+      }
+    ]
+  },
+  {
+    provider: 'BSNL',
+    state: 'Andhra Pradesh',
+    person: [
+      {
+        name: 'Menon',
+        City: 'BZA',
+        phone: '2345617230'
+      },
+      {
+        name: 'Mayank',
+        City: 'Rajamandry',
+        phone: '4567292021'
+      }
+    ]
+  },
+  {
+    provider: 'Airtel',
+    state: 'Andhra Pradesh',
+    person: [
+      {
+        name: 'Su',
+        City: 'Vizag',
+        phone: '987654321'
+      },
+      {
+        name: 'Srini',
+        City: 'Ongol',
+        phone: '2345678901'
+      }
+    ]
+  },
+  {
+    provider: 'BSNL',
+    state: 'Karnataka',
+    person: [
+      {
+        name: 'Menon',
+        City: 'BZA',
+        phone: '2345617230'
+      },
+      {
+        name: 'Mayank',
+        City: 'Rajamandry',
+        phone: '4567292021'
+      }
+    ]
+  }
+];
+
+export const Providers = [
+  { value: 'Airtel', label: 'Airtel' },
+  { value: 'Jio', label: 'Jio' },
+  { value: 'BSNL', label: 'BSNL' }
+];
+
+export const States = [
+  { value: 'Telangana', label: 'Telangana' },
+  { value: 'Karnataka', label: 'Karnataka' },
+  { value: 'AndhraPradesh', label: 'AndhraPradesh' }
+];
